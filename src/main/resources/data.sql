6a35c93c5bb7052e4edc08be2c2bb35490587bba
patcharin063 <61050893@kmitl.ac.th>
2020-11-18 23:09:39 +0700

    fix command,test more data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO PLACE VALUES (1,'D', 'Hello','Hello','Hello');
-INSERT INTO PLACE(place_name,province,category,link) VALUES('วัดลิงขบ','กรุงเทพ','วัด','www.aaa.com')
+INSERT INTO PLACE(place_name,province,category,link) VALUES('วัดลิงขบ','กรุงเทพ','วัด','www.aaa.com');
+INSERT INTO PLACE(place_name,province,category,link) VALUES('1วัดลิงขบ','กรุงเทพ','วัด','www.aaa.com');
+INSERT INTO PLACE(place_name,province,category,link) VALUES('2วัดลิงขบ','กรุงเทพ','วัด','www.aaa.com');
+INSERT INTO PLACE(place_name,province,category,link) VALUES('ว3ัดลิงขบ','กรุงเทพ','วัด','www.aaa.com');
+INSERT INTO PLACE(place_name,province,category,link) VALUES('4วัดลิงขบ','กรุงเทพ','วัด','www.aaa.com');
